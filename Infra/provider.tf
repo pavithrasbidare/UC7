50c36f9123d4f54b8cf7dfbce13962ee105e5b84
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 04:49:13 +0530

    Create provider.tf

---
+++ Infra/provider.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.eks.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
+
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = jsonencode([
+      {
+        rolearn = module.iam.node_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+    mapUsers = jsonencode([
+  {
+    userarn = "arn:aws:iam::183114607892:user/terraform-user"
+    username = "terraform-user"
+    groups = [
+      "system:masters"
+    ]
+  }
+])
+}
+}
