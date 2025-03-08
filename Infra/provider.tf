654a782b310ea353f2fecc0fca175befee5eb95f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 04:50:04 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -2,6 +2,26 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   token                  = data.aws_eks_cluster_auth.eks.token
